32a259fae3f6cd7aad5999972835497cd39222b8
hyl317 <yh362@cornell.edu>
2021-02-16 23:27:39 -0500

    save current veresion; start working on pools

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ -O3 -Wall -Wextra -pthread -I/fs/cbsubscb09/storage/yilei/tools/lib/eigen-3.3.7/ -I/fs/cbsubscb09/storage/yilei/tools/lib/boost_1_66_0 -lz -o DRUID *.cpp
+g++ -pg -O3 -Wall -Wextra -pthread -I/fs/cbsubscb09/storage/yilei/tools/lib/eigen-3.3.7/ -I/fs/cbsubscb09/storage/yilei/tools/lib/boost_1_66_0 -lz -o DRUID *.cpp
